54a82628a34032c36772ed4836e6ace77c9c00cf
DaniloGB91 <dani.celis@duocuc.cl>
2025-05-15 16:29:08 -0400

    Se Realiza Microservicio De Envio a RamaDanilo

---
+++ src/main/java/com/infinityparfum/InfinityParfumApplication.java
@@ -0,0 +1,13 @@
+package com.infinityparfum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfinityParfumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfinityParfumApplication.class, args);
+	}
+
+}
