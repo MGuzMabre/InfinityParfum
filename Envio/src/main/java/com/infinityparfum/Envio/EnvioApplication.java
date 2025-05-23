ca4f7b564309f7c315f6536e56704874faddb1aa
MGuzMabre <matia.guzmana@duocuc.cl>
2025-05-23 09:22:26 -0400

    Modificacion de Application Test y Envio Application

--- Envio/src/main/java/com/infinityparfum/Envio/EnvioApplication.java
+++ Envio/src/main/java/com/infinityparfum/Envio/EnvioApplication.java
@@ -2,8 +2,7 @@ package com.infinityparfum.Envio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication(scanBasePackages = "com.infinityparfum.Envio")
 public class EnvioApplication {
@@ -12,8 +11,4 @@ public class EnvioApplication {
         SpringApplication.run(EnvioApplication.class, args);
     }
 
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
 }
