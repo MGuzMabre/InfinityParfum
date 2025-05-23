dab71f65f68eec185bb5ffce992e4eb85a98838a
MGuzMabre <matia.guzmana@duocuc.cl>
2025-05-23 09:27:44 -0400

    Mod envioTest

--- Envio/src/test/java/com/infinityparfum/Envio/EnvioApplicationTests.java
+++ Envio/src/test/java/com/infinityparfum/Envio/EnvioApplicationTests.java
@@ -2,9 +2,16 @@ package com.infinityparfum.Envio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ActiveProfiles("test") // Usa perfil test
 class EnvioApplicationTests {
+
+    @MockitoBean
+    private RestTemplate restTemplate; // Simula RestTemplate
 
     @Test
     void contextLoads() {
